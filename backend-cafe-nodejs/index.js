3a6fb4be8213e96e11a6b3e189c4cb06fa338522
julianopedraca <juliano.pedraca@gmail.com>
2023-03-16 18:29:43 -0300

    feat: generate bill pdf

--- backend-cafe-nodejs/index.js
+++ backend-cafe-nodejs/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 var cors = require("cors");
-const connection = require("./connection");
 const userRoute = require("./routes/user");
 const categoryRoute = require("./routes/category");
+const productRoute = require("./routes/product");
+const billRoute = require("./routes/bill");
 const app = express();
 
 app.use(cors());
@@ -10,5 +11,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use("/user", userRoute);
 app.use("/category", categoryRoute);
+app.use("/product", productRoute);
+app.use("/bill", billRoute);
 
 module.exports = app;
--- backend-cafe-nodejs/table.sql
+++ backend-cafe-nodejs/table.sql
@@ -33,3 +33,39 @@ CREATE TABLE category(
 --@block
 SELECT * FROM category;
 
+--@block
+CREATE TABLE product(
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    description VARCHAR (255),
+    price INTEGER,
+    status VARCHAR(20),
+    PRIMARY KEY (id)
+);
+
+--@block
+SELECT * FROM product;
+
+--@block
+ALTER TABLE product
+ADD categoryId INTEGER NOT NULL ;
+
+--@block
+CREATE TABLE bill(
+    id INT NOT NULL AUTO_INCREMENT,
+    uuid VARCHAR (200) NOT NULL,
+    name VARCHAR (255) NOT NULL,
+    email VARCHAR (255) NOT NULL,
+    contactNumber VARCHAR(20) NOT NULL,
+    paymentMerhod VARCHAR(50) NOT NULL,
+    total INT NOT NULL,
+    productDetails JSON DEFAULT NULL,
+    createdBy VARCHAR(255) NOT NULL,
+    PRIMARY KEY (id)
+);
+
+--@block
+DESC bill
+
+--@block
+SELECT * FROM bill;
