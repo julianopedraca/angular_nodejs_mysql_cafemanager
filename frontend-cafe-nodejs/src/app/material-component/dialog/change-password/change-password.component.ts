a4829aef41912f3888ffd6afdcad963b85818674
julianopedraca <juliano.pedraca@gmail.com>
2023-03-31 16:52:02 -0300

    anguar.json budget 2kb to 200kb and 4kb to 400kb

---
+++ frontend-cafe-nodejs/src/app/material-component/dialog/change-password/change-password.component.ts
@@ -0,0 +1,75 @@
+import { SnackbarService } from './../../../services/snackbar.service';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatDialogRef } from '@angular/material/dialog';
+import { UserService } from 'src/app/services/user.service';
+import { NgxUiLoaderService } from 'ngx-ui-loader';
+import { GlobalConstants } from 'src/app/shared/global-constants';
+
+@Component({
+  selector: 'app-change-password',
+  templateUrl: './change-password.component.html',
+  styleUrls: ['./change-password.component.scss']
+})
+export class ChangePasswordComponent implements OnInit{
+
+  changePasswordForm:any = FormGroup;
+  responseMessage:any
+
+  constructor(private formBuilder:FormBuilder,
+    private userService:UserService,
+    public dialogRef:MatDialogRef<ChangePasswordComponent>,
+    private ngxService:NgxUiLoaderService,
+    private snackbarService: SnackbarService){}
+
+  ngOnInit(): void {
+    this.changePasswordForm = this.formBuilder.group({
+      oldPassword: [null,[Validators.required]],
+      newPassword: [null,[Validators.required]],
+      confirmPassword: [null,[Validators.required]],
+    })
+  }
+
+  validateSubmit(){
+   if (this.changePasswordForm.controls['newPassword'].value != this.changePasswordForm.controls['confirmPassword'].value){
+    return true
+   } else {
+    return false
+   }
+  }
+
+  handleChangePasswordSubmit(){
+    this.ngxService.start()
+    var formData = this.changePasswordForm.value
+    var data = {
+      oldPassword: formData.oldPassword,
+      newPassword:formData.newPassword,
+      confirmPassword: formData.confirmPassword 
+    }
+
+    this.userService.changePassword(data).subscribe({
+      next: (response:any)=>{
+        this.ngxService.stop()
+        this.responseMessage = response?.message;
+        this.dialogRef.close()
+        this.snackbarService.openSnackBar(this.responseMessage,"Success")
+      },
+      error:(error)=>{
+        console.log(error)
+        this.ngxService.stop();
+        if(error.error?.message){
+          this.responseMessage = error.error?.message
+        } else {
+          this. responseMessage = GlobalConstants.genericError
+        }
+        this.snackbarService.openSnackBar(this.responseMessage, GlobalConstants.error)
+      }
+    })
+  }
+
+  //debug -> Delete Later
+  printChangePassword(){
+    console.log(this.changePasswordForm)
+  }
+
+}
